7b8d936ab1a868640084be14ec5b8b076ca3a815
Matsuoka Ryota <saymr731@gmail.com>
2024-08-23 14:47:11 +0900

    kadai_005/bmi.jsを追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+// 変数の宣言・代入
+let bodyWeight=68;
+let height=1.7;
+//BMI確認のための計算  BMI　体重　/　(身長)²
+let BMI=bodyWeight/(height*height);
+
+// 出力
+console.log(bodyWeight);
+console.log(height);
+console.log(BMI)
+
+
+
