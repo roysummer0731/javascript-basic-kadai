a13e592749248950a95c8aea79f4ba7cae769dee
Matsuoka Ryota <saymr731@gmail.com>
2024-08-24 22:00:40 +0900

    kadai_007/calc. 追加

---
+++ kadai_007/calc.js
@@ -0,0 +1,24 @@
+let num=8;
+
+switch(true){
+  case num % 3 == 0 && num % 5 == 0:
+  console.log('３と５の倍数です');
+  break;
+
+  case num % 3 == 0:
+  console.log('３の倍数です');
+  break;
+  
+  case num % 5 == 0:
+  console.log('５の倍数です');
+  break;
+  
+  default: console.log(num);
+  break;
+  
+
+}
+
+
+
+
