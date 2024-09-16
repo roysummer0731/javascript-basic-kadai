e75987bf9704a9ce15a258838bc439c7d5d9c6ce
Matsuoka Ryota <saymr731@gmail.com>
2024-09-16 10:27:24 +0900

    mdn.jsを追加

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,8 @@
-// 2023年8月22日のDateオブジェクトを作成
-const date = new Date(2023, 7, 22);
+// Dateオブジェクトを作成
+const date = new Date();
 
 // 日付をフォーマットして表示
 const toDay = date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日';
 
-console.log(toDay); // 2023年8月22日
-
+// 今日の日付
+console.log(toDay); 
